258c9f8cc94427136fc04b177539b23ae20990ac
Pat <a03991358@gmail.com>
2026-03-03 22:16:31 +0000

    Update app.php
    added middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        'rate.limit' => \App\Http\Middleware\RateLimitRequests::class,
+        'brute.force' => \App\Http\Middleware\PreventBruteForce::class,
+        'honeypot' => \App\Http\Middleware\Honeypot::class,
+        'admin.ip' => \App\Http\Middleware\SecureAdminIpWhitelist::class,
+        'admin.auth' => \App\Http\Middleware\AdminAuthCheck::class,
+        'file.upload' => \App\Http\Middleware\ValidateFileUpload::class,
+        'validate.slug' => \App\Http\Middleware\ValidateSlug::class,
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
